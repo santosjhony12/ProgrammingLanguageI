8437e9865a6ddb13221eeaa2e46aa6e31789cdd5
santosjhony12 <santosdesouzajhony@gmail.com>
2023-08-10 21:34:03 -0300

    Class 1

--- Class01/src/main/java/org/example/Main.java
+++ Class01/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int a;
+       int b;
+       a = 5;
+       b = 10;
+       int soma = a+b;
+       System.out.println("Soma = "+soma);
     }
 }
