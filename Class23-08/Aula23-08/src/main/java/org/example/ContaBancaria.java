dec917f4cee5f5d5ffaba38c812f35caeeb99b23
santosjhony12 <santosdesouzajhony@gmail.com>
2023-08-23 22:13:39 -0300

    Classe ContaBancaria

---
+++ Class23-08/Aula23-08/src/main/java/org/example/ContaBancaria.java
@@ -0,0 +1,57 @@
+package org.example;
+public class ContaBancaria {
+    private String titular;
+    private double saldo;
+    private String tipoConta;
+
+    public ContaBancaria() {
+    }
+
+    public ContaBancaria(String titular, double saldoInicial, String tipoConta) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+        this.tipoConta = tipoConta;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito realizado. Novo saldo: R$" + saldo);
+    }
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque realizado. Novo saldo: R$" + saldo);
+        } else {
+            System.out.println("Saldo insuficiente para o saque.");
+        }
+    }
+
+    public void exibirSaldo() {
+        System.out.println("Saldo atual: R$" + saldo);
+    }
+}
---
+++ Class23-08/Aula23-08/src/main/java/org/example/Livro.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Livro: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Ano de Publicação: " + anoPublicacao);
+    }
+
+    public void emprestar() {
+        System.out.println("Livro emprestado para leitura.");
+    }
+
+    public void devolver() {
+        System.out.println("Livro devolvido.");
+    }
+}
