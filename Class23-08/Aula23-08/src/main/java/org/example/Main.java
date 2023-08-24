7239ecd44de056756fe64d81b7a40ee98e9d5b8b
santosjhony12 <santosdesouzajhony@gmail.com>
2023-08-23 22:03:57 -0300

    Classe Carro

---
+++ Class23-08/Aula23-08/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Class23-08/Aula23-08/src/main/java/org/example/TesteCarro.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class TesteCarro {
+    public static void main(String[] args){
+        /*INSTANCIA DO OBJETO COM CONSTRUTOR DEFINIDO*/
+        Carro carro1 = new Carro(12, "ABC123", "Azul","FIAT", "UNO" );
+
+        /*impressões dos atributos do objeto carro1 utilizando a classe Carro*/
+        System.out.println("Velocidade Atual: "+carro1.getVelocidadeAtual());
+        System.out.println("Placa: "+carro1.getPlaca());
+        System.out.println("Cor: "+carro1.getCor());
+        System.out.println("Marca: "+carro1.getMarca());
+        System.out.println("Modelo: "+carro1.getModelo());
+        carro1.andar(144);
+        carro1.freiar();
+    }
+}
