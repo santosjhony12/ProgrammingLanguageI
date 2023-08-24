f86b38a5b23a4cc7eacc7c9512a33aa0d37033a3
santosjhony12 <santosdesouzajhony@gmail.com>
2023-08-23 22:15:41 -0300

    Classe Produto

---
+++ Class23-08/Aula23-08/src/main/java/org/example/Produto.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int estoque;
+
+    public Produto(String nome, double preco, int estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+    public void exibirDetalhes() {
+        System.out.println("Produto: " + nome);
+        System.out.println("Preço: R$" + preco);
+        System.out.println("Estoque: " + estoque + " unidades");
+    }
+
+    public void vender(int quantidade) {
+        if (estoque >= quantidade) {
+            estoque -= quantidade;
+            System.out.println("Venda realizada. Novo estoque: " + estoque);
+        } else {
+            System.out.println("Estoque insuficiente para a venda.");
+        }
+    }
+}
+
